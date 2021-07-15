a625b38803c1aaef9cf9087e3e24684458d72538
deanna <42.dpiedra@gmail.com>
2021-07-15 13:48:03 +0200

    color options are now done

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 const container = document.querySelector("#container");
 let gridSize = 0;
 
-function changeColor(e) {
-    const randomR = Math.floor(Math.random() * 256);
-    const randomG = Math.floor(Math.random() * 256);
-    const randomB = Math.floor(Math.random() * 256);
-    e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+function changeColor(e, color) {
+
+    if (color == 'Pink') {
+        const pinkR = 255;
+        const pinkG = 192;
+        const pinkB = 203;
+        e.target.style.backgroundColor = `rgb(${pinkR}, ${pinkG}, ${pinkB})`;
+    }
+    else if (color == 'Rainbow'){
+        const randomR = Math.floor(Math.random() * 256);
+        const randomG = Math.floor(Math.random() * 256);
+        const randomB = Math.floor(Math.random() * 256);
+        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+    }
+    else if (color == 'Lightpink') {
+        const lightpinkR = 250;
+        const lightpinkG = 234;
+        const lightpinkB = 244;
+        e.target.style.backgroundColor = `rgb(${lightpinkR}, ${lightpinkG}, ${lightpinkB})`;
+    }
   }
+
+function getColor(e) {
+    let color = document.querySelector('input[name="color_choice"]:checked').value;
+
+    changeColor(e, color);
+}
 
 function clearGrid() {
     const gridArray = Array.from(container.childNodes);
@@ -36,7 +57,7 @@ function createGrid(size) {
     for (let i = 0; i < size * size; i++) {
         const gridElement = document.createElement("div");
         gridElement.className = "grid-square";
-        gridElement.addEventListener('mouseover', e => e.target.classList.add('pinkColor'));
+        gridElement.addEventListener('mouseover', getColor);
         container.appendChild(gridElement);
     }
 }
